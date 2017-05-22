0b3fcc15fd2ab64e004acae2b51116cb85223d31
Thomas Falkowski <thomas.falkowski@nsidc.org>
2017-05-22 10:53:27 -0600

    Update service.pp
    Adding code to support systemd. This can no doubt be refactored and simplified.

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,17 +2,28 @@
 #
 # Optionally manage the Telegraf service.
 #
-class telegraf::service {
+lass telegraf::service {
 
   assert_private()
 
   if $::telegraf::manage_service {
-    service { 'telegraf':
-      ensure    => $telegraf::service_ensure,
-      hasstatus => $telegraf::service_hasstatus,
-      enable    => $telegraf::service_enable,
-      restart   => $telegraf::service_restart,
-      require   => Class['::telegraf::config'],
-    }
+    if ($::osfamily == 'RedHat' and $::operatingsystemmajrelease == '7') or ($::operatingsystem == 'Ubuntu' and $::operatingsystemmajrelease >= '15.10') {
+      service { 'telegraf':
+        ensure    => $telegraf::service_ensure,
+        provider  => systemd,
+        hasstatus => $telegraf::service_hasstatus,
+        enable    => $telegraf::service_enable,
+        restart   => $telegraf::service_restart,
+        require   => Class['::telegraf::config'],
+      }
+    }else {
+      service { 'telegraf':
+        ensure    => $telegraf::service_ensure,
+        hasstatus => $telegraf::service_hasstatus,
+        enable    => $telegraf::service_enable,
+        restart   => $telegraf::service_restart,
+        require   => Class['::telegraf::config'],
+      }
+    }  
   }
 }
